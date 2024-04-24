36c34ea4e5a1e84041375e849a507cc6a53c7f36
AnakovaK <101050346+AnakovaK@users.noreply.github.com>
2024-04-24 13:55:59 +0300

    Update Collectable.cs

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Класс реализует предметы, которые можно подобрать в инвентарь
+/// </summary>
 [RequireComponent(typeof(Item))]
 public class Collectable : MonoBehaviour
 {
+    /// <summary>
+    /// Этот метод передаёт информацию о коллизии с игроком.
+    /// </summary>
+    /// <param name="collision">Параметр коллайдера предмета, с чем сталкивается предмет</param>
+    /// <returns></returns>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
